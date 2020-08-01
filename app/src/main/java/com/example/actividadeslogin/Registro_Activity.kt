fec428e9bf6e54c36af2771ba5b71fbc1c8ab96b
ricardo-cuesta <ricardo.cuesta@udea.edu.co>
2020-08-01 11:36:34 -0500

    login

--- app/src/main/java/com/example/actividadeslogin/Registro_Activity.kt
+++ app/src/main/java/com/example/actividadeslogin/Registro_Activity.kt
@@ -1,31 +1,80 @@
 package com.example.actividadeslogin
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.content.Intent
+import android.icu.text.MessageFormat.format
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.DateFormat.format
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.DatePicker
 import kotlinx.android.synthetic.main.activity_registro.*
+import java.lang.String.format
+import java.text.MessageFormat.format
+import java.text.SimpleDateFormat
+import java.time.MonthDay
+import java.util.*
+import java.util.Locale.US
+import java.util.Locale.forLanguageTag
+import kotlinx.android.synthetic.main.activity_registro.tv_fecha_nacimiento as tv_fecha_nacimiento1
 
 class Registro_Activity : AppCompatActivity() {
+    private lateinit var date:String
+    private var cal = Calendar.getInstance()
+
+
+
+
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
 
        // val bt_aceptar : Button = findViewById(R.id.bt_aceptar)
 
-        bt_aceptar.setOnClickListener{
 
-            val nombre =et_nombre.text.toString()
-            val cedula =et_cedula.text.toString()
-            val correo = et_correo.text.toString()
-
-
-            tv_resumen.text="nombre: $nombre  \ndocumento:$cedula \ncorreo: $correo \n "
-
+        val dateSetListener= object :DatePickerDialog.OnDateSetListener
+        {
+            override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
+                cal.set(Calendar.YEAR,year)
+                cal.set(Calendar.MONTH,month)
+                cal.set(Calendar.DAY_OF_MONTH,day)
+                val format="dd/MM/yyyy"
+                val simpleDateFormat=SimpleDateFormat(format, US)
+                var fecha = simpleDateFormat.format(cal.time).toString()
+                tv_fecha_nacimiento1.text=fecha
+            }
 
         }
+        ib_calendario.setOnClickListener {
+            DatePickerDialog( this,
+                dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
 
+
+
+        bt_aceptar.setOnClickListener{ it: View? ->
+
+            val nombre = et_nombre.text.toString()
+            val cedula = et_cedula.text.toString()
+            val correo = et_correo.text.toString()
+            val password =et_password.text.toString()
+            var fecha :String = tv_fecha_nacimiento1.text.toString()
+            val password_confirma=et_confirma_password.text.toString()
+
+            if ((nombre!="")&&(cedula!="")&&(correo!="")&&(fecha!="")&&(password!="")){
+                if (password ==password_confirma) {
+                tv_resumen.text = "nombre: $nombre  \ndocumento:$cedula \ncorreo: $correo \nfecha de nacimiento: $fecha"
+                }else{tv_resumen.text = "la contraseña no coincide $password"}
+            }
+        }
 
 }
 
@@ -45,6 +94,8 @@ class Registro_Activity : AppCompatActivity() {
             startActivity(intent)        }
         return super.onOptionsItemSelected(item)
     }
+
+    fun OnCalendarButtonClicked(view: View) {}
 
 
 }
